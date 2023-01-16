762bc839894d1a93a48d9d35c301c49da97f9ad2
Ângela Oliveira <89156781+anjinha-oliveira@users.noreply.github.com>
2023-01-16 14:41:11 -0300

    Implementando o script HTML e CSS

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section class="content">
+        <h1>Pokedex</h1>
+        <ol id="pokemonList" class="pokemons">
+        <li class="pokemon grass">
+                <span class="number">#1</span>
+                <span class="name">bulbasaur</span>
+                    <div class="detail">
+                        <ol class="types">
+                            <li class="type grass">grass</li><li class="type poison">poison</li>
+                        </ol>        
+                        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" alt="bulbasaur"/>
+                    </div>
+            </li>
+            <li class="pokemon grass">
+                <span class="number">#2</span>
+                <span class="name">ivysaur</span>
+                    <div class="detail">
+                        <ol class="types">
+                            <li class="type grass">grass</li><li class="type poison">poison</li>
+                        </ol>        
+                        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/2.svg" alt="ivysaur"/>
+                    </div>
+            </li>
+        </ol>
+        <div class="pagination">
+            <button id="loadMoreButton" type="button">
+                load more
+            </button>
+        </div>
+    </section>
   );
 }
-
 export default App;
