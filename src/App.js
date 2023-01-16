f7d0d8a132f49443402cef99b72c62e2040f42ba
Ângela Oliveira <89156781+anjinha-oliveira@users.noreply.github.com>
2023-01-16 17:00:14 -0300

    Recebendo dados da api

--- src/App.js
+++ src/App.js
@@ -1,32 +1,50 @@
+import React, { useEffect, useState } from 'react';
+
+
 import './App.css';
+import pokeApi from './api'
 
 function App() {
+
+  const limit = 5
+  const offset = 0
+
+  const [pokemons, setPokemons] = useState([]);
+
+  useEffect(() => {
+    pokeApi.getPokemons(offset, limit).then((pokemonsData = []) => {
+      setPokemons(pokemonsData)
+    })
+  }, [])
+  
+  console.log(pokemons)
+  
   return (
-    <section class="content">
-        <h1>Pokedex</h1>
-        <ol id="pokemonList" class="pokemons">
-        <li class="pokemon grass">
-                <span class="number">#1</span>
-                <span class="name">bulbasaur</span>
-                    <div class="detail">
-                        <ol class="types">
-                            <li class="type grass">grass</li><li class="type poison">poison</li>
-                        </ol>        
-                        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" alt="bulbasaur"/>
-                    </div>
-            </li>
-            <li class="pokemon grass">
-                <span class="number">#2</span>
-                <span class="name">ivysaur</span>
-                    <div class="detail">
-                        <ol class="types">
-                            <li class="type grass">grass</li><li class="type poison">poison</li>
-                        </ol>        
-                        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/2.svg" alt="ivysaur"/>
-                    </div>
+    <section className="content">
+      <h1>Pokedex</h1>
+      <ol id="pokemonList" className="pokemons">
+        <li className="pokemon grass">
+          <span className="number">#1</span>
+          <span className="name">bulbasaur</span>
+          <div className="detail">
+              <ol className="types">
+                <li className="type grass">grass</li><li className="type poison">poison</li>
+              </ol>        
+              <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" alt="bulbasaur"/>
+          </div>
+        </li>
+            <li className="pokemon grass">
+              <span className="number">#2</span>
+              <span className="name">ivysaur</span>
+                <div className="detail">
+                  <ol className="types">
+                      <li className="type grass">grass</li><li className="type poison">poison</li>
+                  </ol>        
+                  <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/2.svg" alt="ivysaur"/>
+                </div>
             </li>
         </ol>
-        <div class="pagination">
+        <div className="pagination">
             <button id="loadMoreButton" type="button">
                 load more
             </button>
