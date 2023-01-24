cf30bb7f34a27abaf46ddf547a20aceb9c5ba81b
Ângela Oliveira <89156781+anjinha-oliveira@users.noreply.github.com>
2023-01-24 16:30:36 -0300

    Fazendo o alinhamento do botão "voltar"

--- src/pages/PokeTypes.js
+++ src/pages/PokeTypes.js
@@ -3,21 +3,25 @@ import { useParams } from 'react-router-dom';
 import { Link } from "react-router-dom";
 
 function PokeTypes() {
-    let {number} = useParams()
-    return (
-        <section className="content">
-            <div>
-                <Link to="/">
-                <button 
+  let {number} = useParams()
+  return (
+    <section className="content">
+      <div>
+          <h1>Pokemon nº{number}</h1>
+        <div>
+          <div class="d-flex justify-content-end">
+            <Link to="/">
+              <button 
                 type="button" 
                 className="btn btn-outline-info">
-                    Voltar
-                </button>    
-                </Link>    
-                <h1> oi</h1>
-            </div>
-        </section>
-    )
+                  Voltar
+              </button>    
+            </Link>
+            </div>    
+          </div>
+        </div>      
+    </section>
+  )
 }
 
 export default PokeTypes;
