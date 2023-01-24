689e28b779d2d8532e944fc56d1886fd0bbb358e
Ângela Oliveira <89156781+anjinha-oliveira@users.noreply.github.com>
2023-01-24 15:57:08 -0300

    Adicionando o botão voltar na página de tipos de pokemon  com link para voltar  á pagina inicial

--- src/pages/PokeTypes.js
+++ src/pages/PokeTypes.js
@@ -1,10 +1,23 @@
 import React from "react";
-
-
+import { useParams } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 function PokeTypes() {
-    
-    return <h1> pokeTypes </h1>
+    let {number} = useParams()
+    return (
+        <section className="content">
+            <div>
+                <Link to="/">
+                <button 
+                type="button" 
+                className="btn btn-outline-info">
+                    Voltar
+                </button>    
+                </Link>    
+                <h1> oi</h1>
+            </div>
+        </section>
+    )
 }
 
 export default PokeTypes;
